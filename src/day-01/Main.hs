26853073a1248549674444446c425f2175cffaab
John Martinez <john@demiurgemgmt.com>
2022-12-01 13:55:48 -0400

    added makefile and style guide (#4)

--- src/day-01/Main.hs
+++ src/day-01/Main.hs
@@ -9,4 +9,4 @@ main = do
   let cals = init $ splitOn "\n\n" calories
   let ans = map (sum . map (\ w -> read w :: Int) . splitOn "\n") cals
   print $ maximum ans
-  print $ sum $ take 3 $ (reverse. sort) ans
+  print $ sum $ take 3 $ (reverse . sort) ans
