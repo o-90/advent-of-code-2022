e5b38dde15a26d8aa09ded444a20197eb0a41d81
o-90 <john@demiurgemgmt.com>
2022-12-03 15:15:10 +0000

    finished part one

--- src/day-03/Main.hs
+++ src/day-03/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import Data.List (splitAt)
+import Control.Monad (sequence)
+import qualified Data.Map as M
+
+createDict :: String -> M.Map Char Int
+createDict s = M.fromList $ zip s [1..]
+
+processStr :: M.Map Char Int -> String -> Maybe Int
+processStr m s = M.lookup (head k) m
+  where
+    k = M.keys $ M.intersection ld rd
+    rd = createDict ri
+    ld = createDict li
+    (li, ri) = splitAt (length s `div` 2) s
+
 main :: IO ()
-main = undefined
+main = do
+  -- get input data
+  inputs <- getContents
+  -- map priority to items
+  let priorityDict = createDict (['a'..'z'] ++ ['A'..'Z'])
+  -- partially apply string processor with the priorities
+  let processStrPriority = processStr priorityDict
+  -- process strings
+  let output = map processStrPriority (lines inputs)
+  -- get sum
+  print $ sum <$> sequence output
