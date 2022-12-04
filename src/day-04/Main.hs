801caf01940179dac8149b8d74d0e0b126ebc02d
o-90 <john@demiurgemgmt.com>
2022-12-04 11:16:48 -0400

    finished day four

--- src/day-04/Main.hs
+++ src/day-04/Main.hs
@@ -8,12 +8,24 @@ checkPairs s
   | la <= lb && ra >= rb = 1
   | otherwise            = 0
   where
-    [la, ra] = map (\ w -> read w :: Int) l
-    [lb, rb] = map (\ w -> read w :: Int) r
-    [l,r] = map (splitOn "-") . splitOn "," $ s
+    [la, ra] = map (\ w -> read w :: Int) a
+    [lb, rb] = map (\ w -> read w :: Int) b
+    [a,b] = map (splitOn "-") . splitOn "," $ s
+
+checkPairs' :: String -> Int
+checkPairs' s
+  | la < rb && ra < lb = 0
+  | la > rb && ra > lb = 0
+  | otherwise          = 1
+  where
+    [la, ra] = map (\ w -> read w :: Int) a
+    [lb, rb] = map (\ w -> read w :: Int) b
+    [a,b] = map (splitOn "-") . splitOn "," $ s
 
 main :: IO ()
 main = do
   pairs <- getContents
-  let example = head $ lines pairs
+  -- part one
   print $ sum $ map checkPairs (lines pairs)
+  -- part two
+  print $ sum $ map checkPairs' (lines pairs)
