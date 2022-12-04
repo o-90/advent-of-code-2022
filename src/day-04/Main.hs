08f152351b7f3f0ecc14526b1b61246688613cd7
o-90 <john@demiurgemgmt.com>
2022-12-04 11:01:13 -0400

    day four part one

--- src/day-04/Main.hs
+++ src/day-04/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Data.List.Split (splitOn)
+
+checkPairs :: String -> Int
+checkPairs s
+  | la >= lb && ra <= rb = 1
+  | la <= lb && ra >= rb = 1
+  | otherwise            = 0
+  where
+    [la, ra] = map (\ w -> read w :: Int) l
+    [lb, rb] = map (\ w -> read w :: Int) r
+    [l,r] = map (splitOn "-") . splitOn "," $ s
+
 main :: IO ()
-main = undefined
+main = do
+  pairs <- getContents
+  let example = head $ lines pairs
+  print $ sum $ map checkPairs (lines pairs)
