fc845afe9ac7c730ab70ac934f1501ca6fe57c34
John Martinez <john@demiurgemgmt.com>
2022-12-05 15:32:07 -0400

    just ended up using listss (#12)

--- src/day-05/Main.hs
+++ src/day-05/Main.hs
@@ -1,35 +1,37 @@
 module Main where
 
-import           Control.Monad (foldM)
 import qualified Data.Map as M
 import           Data.Maybe (fromJust)
-import           Data.Stack (Stack, stackNew, stackPop, stackPush)
 
-type Crate = Stack String
+type Crate = [String]
 type Cache = M.Map Int Crate
-
-loadCrate :: [String] -> Crate
-loadCrate = foldl stackPush stackNew
 
 createDict :: [Crate] -> Cache
 createDict c = M.fromList $ zip [1..] c
-
-popAndPush :: (Crate, Crate) -> (Crate, Crate)
-popAndPush (ca, cb) = case stackPop ca of
-  Just (nStack, nItem) -> (nStack, stackPush cb nItem)
-  _                    -> (ca, cb)
 
 updateCrates :: Cache -> Int -> Int -> Int -> Cache
 updateCrates m k f t = m''
   where
     m'' = M.insert t ocb m'
     m' = M.insert f oca m
-    (oca, ocb) = iterate popAndPush (ca, cb) !! k
+    (oca, ocb) = (drop k ca, (reverse . take k $ ca) ++ cb)
+    ca = fromJust $ M.lookup f m
+    cb = fromJust $ M.lookup t m
+
+updateCrates' :: Cache -> Int -> Int -> Int -> Cache
+updateCrates' m k f t = m''
+  where
+    m'' = M.insert t ocb m'
+    m' = M.insert f oca m
+    (oca, ocb) = (drop k ca, take k ca ++ cb)
     ca = fromJust $ M.lookup f m
     cb = fromJust $ M.lookup t m
 
 runner :: Cache -> [Int] -> Cache
 runner c [x, y, z] = updateCrates c x y z
+
+runner' :: Cache -> [Int] -> Cache
+runner' c [x, y, z] = updateCrates' c x y z
 
 main :: IO ()
 main = do
@@ -45,7 +47,7 @@ main = do
   input9 <- getLine
 
   -- TODO(o-90): this could be better
-  let crateMap = createDict $ map (loadCrate . words) [input1, input2, input3, input4, input5, input6, input7, input8, input9]
+  let crateMap = createDict $ map words [input1, input2, input3, input4, input5, input6, input7, input8, input9]
 
   -- get instructions
   rawInstructions <- getContents
@@ -53,3 +55,6 @@ main = do
 
   -- part one answer
   print $ foldl runner crateMap instructions
+
+  -- part two answer
+  print $ foldl runner' crateMap instructions
