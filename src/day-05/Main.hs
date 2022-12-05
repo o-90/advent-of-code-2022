8a7f8402256d3bacf4a4b42c9a3ee83543ecb489
John Martinez <john@demiurgemgmt.com>
2022-12-05 15:43:59 -0400

    day 5 done (#13)
    * the input was a bit cumbersome to parse so did some preprocessing
    
    * parsing input
    
    * day 5 part one
    
    * just ended up using listss (#12)
    
    * finished day 5

--- src/day-05/Main.hs
+++ src/day-05/Main.hs
@@ -1,4 +1,62 @@
 module Main where
 
+import qualified Data.Map as M
+import           Data.Maybe (fromJust)
+
+type Crate = [String]
+type Cache = M.Map Int Crate
+
+createDict :: [Crate] -> Cache
+createDict c = M.fromList $ zip [1..] c
+
+updateCrates :: Cache -> Int -> Int -> Int -> Cache
+updateCrates m k f t = m''
+  where
+    m'' = M.insert t ocb m'
+    m' = M.insert f oca m
+    (oca, ocb) = (drop k ca, (reverse . take k $ ca) ++ cb)
+    ca = fromJust $ M.lookup f m
+    cb = fromJust $ M.lookup t m
+
+updateCrates' :: Cache -> Int -> Int -> Int -> Cache
+updateCrates' m k f t = m''
+  where
+    m'' = M.insert t ocb m'
+    m' = M.insert f oca m
+    (oca, ocb) = (drop k ca, take k ca ++ cb)
+    ca = fromJust $ M.lookup f m
+    cb = fromJust $ M.lookup t m
+
+runner :: Cache -> [Int] -> Cache
+runner c [x, y, z] = updateCrates c x y z
+
+runner' :: Cache -> [Int] -> Cache
+runner' c [x, y, z] = updateCrates' c x y z
+
 main :: IO ()
-main = undefined
+main = do
+  -- load stacks
+  input1 <- getLine
+  input2 <- getLine
+  input3 <- getLine
+  input4 <- getLine
+  input5 <- getLine
+  input6 <- getLine
+  input7 <- getLine
+  input8 <- getLine
+  input9 <- getLine
+
+  -- TODO(o-90): this could be better
+  let crateMap = createDict $ map words [input1, input2, input3, input4, input5, input6, input7, input8, input9]
+
+  -- get instructions
+  rawInstructions <- getContents
+  let instructions = map (map(\w -> read w :: Int) . words) . lines $ rawInstructions
+
+  -- part one answer
+  mapM_ (putStr . \(_, y) -> head y) $ M.toList $ foldl runner crateMap instructions
+  putStrLn ""
+
+  -- part two answer
+  mapM_ (putStr . \(_, y) -> head y) $ M.toList $ foldl runner' crateMap instructions
+  putStrLn ""
