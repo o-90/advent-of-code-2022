309adfee3d8f5bb578df36ffb1922ce8029b8eff
o-90 <john@demiurgemgmt.com>
2022-12-05 14:12:13 +0000

    parsing input

--- src/day-05/Main.hs
+++ src/day-05/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
 main :: IO ()
-main = undefined
+main = do
+  -- load stacks
+  stack1 <- getLine
+  stack2 <- getLine
+  stack3 <- getLine
+  stack4 <- getLine
+  stack5 <- getLine
+  stack6 <- getLine
+  stack7 <- getLine
+  stack8 <- getLine
+  stack9 <- getLine
+
+  -- audit
+  print $ words stack1
+  print $ words stack2
+  print $ words stack3
+  print $ words stack4
+  print $ words stack5
+  print $ words stack6
+  print $ words stack7
+  print $ words stack8
+  print $ words stack9
+
+  -- load stack instructions
+  instructions <- getContents
+
+  -- audit
+  print $ lines instructions
