7b8756bb7701ffbaa08c04f2f186c9e3026bd81f
o-90 <john@demiurgemgmt.com>
2022-12-14 12:21:01 -0400

    built tree

--- src/day-07/Main.hs
+++ src/day-07/Main.hs
@@ -1,38 +1,48 @@
 module Main where
 
-data Tree a = Empty | Node a [Tree a]
-  deriving (Show)
+data Tree a = Node {
+    label :: a
+  , size :: Int
+  , subtree :: [Tree a]
+} deriving (Show, Eq, Ord)
 
-data Info = Info {
-    name   :: !String
-  , parent :: !String
-  , size   :: !Int
-} deriving (Show)
+type RoseTree = Tree String
+type Info = (String, String, Int)
 
-type FileTree = Tree Info
+insert :: RoseTree -> Info -> RoseTree
+insert (Node cl cs ct) (np, nl, ns)
+  | cl /= np  = Node cl cs (map insert' ct)
+  | otherwise = Node cl cs (Node nl ns [] : ct)
+  where insert' = flip insert (np, nl, ns)
 
-insert :: FileTree -> Info -> FileTree
-insert Empty i = Node i []
-insert (Node (Info rn rp rs) rt) (Info nn np ns)
-  | rn == np  = Node (Info rn rp (rs + ns)) (Node (Info nn np ns) [] : rt)
-  | otherwise = Node (Info rn rp rs       ) (map insert' rt              )
+rootSeed :: RoseTree
+rootSeed = Node "/" 0 []
+
+-- ---------------------------------------------------------------------------
+-- -- parsing possibilitites
+-- ---------------------------------------------------------------------------
+-- $ cd /         -> push root to stack
+-- $ cd ..        -> pop stack
+-- $ cd [dir]     -> push dir to stack
+-- $ ls           -> list items (move to next command)
+-- dir [name]     -> create a node
+-- [size] [name]  -> create a leaf
+-- ---------------------------------------------------------------------------
+parser :: RoseTree -> [String] -> RoseTree
+parser t s = go t s []
   where
-    insert' = flip insert $ Info nn np ns
-
-foldc :: FileTree -> [String] -> FileTree
-foldc t []       = t
-foldc t (y : ys) = case words y of
-  ["$", x, xs]
-    | xs == "/"  -> foldc (insert t (Info "cd /" "None" 0)) ys
-    | xs == ".." -> foldc t ys
-    | otherwise  -> foldc t ys
-  ["$", xs]      -> foldc t ys
-  [x, xs]
-    | x == "dir" -> foldc t ys
-    | otherwise  -> foldc (insert t (Info xs "cd /" (read x :: Int))) ys
-  _              -> foldc t ys
+    go tree ("$ cd /" : xs) _ = go tree xs ["/"]
+    go tree ("$ cd .." : xs) (_ : zs) = go tree xs zs
+    go tree (x : xs) stack@(z : zs) = case words x of
+      ["$", "cd", p] -> go tree xs (p : stack)
+      ["$", "ls"]    -> go tree xs stack
+      ["dir", p]     -> go (insert tree (z, p, 0)) xs stack
+      [size, name]   -> go (insert tree (z, name, read size :: Int)) xs stack
+      _              -> error "bork!"
+    go tree _ [] = tree
+    go tree [] _ = tree
 
 main :: IO ()
 main = do
   inputs <- getContents
-  print $ foldc Empty $ lines inputs
+  print $ parser rootSeed $ lines inputs
