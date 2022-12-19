7fe681497dcef39a9d3a0b21c184ef44c3376371
John Martinez <john@demiurgemgmt.com>
2022-12-19 15:11:09 -0400

    there are duplicate directories at same level (#16)
    * nodes have correct sizes.  need to filter ones above threshold
    
    * example is right. main is wrong. duplications to address.
    
    * there are duplications in the tree.
    
    * finially got part one.

--- src/day-07/Main.hs
+++ src/day-07/Main.hs
@@ -9,14 +9,43 @@ data Tree a = Node {
 type RoseTree = Tree String
 type Info = (String, String, Int)
 
+-- empty node
+rootSeed :: RoseTree
+rootSeed = Node "/" 0 []
+
+-- construct a rose tree
 insert :: RoseTree -> Info -> RoseTree
 insert (Node cl cs ct) (np, nl, ns)
   | cl /= np  = Node cl cs (map insert' ct)
   | otherwise = Node cl cs (Node nl ns [] : ct)
   where insert' = flip insert (np, nl, ns)
 
-rootSeed :: RoseTree
-rootSeed = Node "/" 0 []
+-- pull out the size of a node
+extract :: RoseTree -> Int
+extract (Node _ s _) = s
+
+-- bottom-up update of the size of each node
+sumTree :: RoseTree -> RoseTree
+sumTree node@(Node _ _ []) = node
+sumTree (Node l s t) = Node l s' t'
+  where t' = fmap sumTree t
+        s' = sum (map extract t') + s
+
+-- find and return list of nodes that meet a criteria
+filterTree :: (Int-> Bool) -> RoseTree -> [Int]
+filterTree _ (Node _ _ []) = []
+filterTree p (Node _ s t)
+  | p s       = s : concatMap ftp t
+  | otherwise = concatMap ftp t
+  where ftp = filterTree p
+
+-- push a node name to the stack
+push :: String -> String -> String
+push l r = l ++ " " ++ r
+
+-- pop the stack when moving back a directory
+pop :: String -> String
+pop = unwords . tail . words
 
 -- ---------------------------------------------------------------------------
 -- -- parsing possibilitites
@@ -29,20 +58,20 @@ rootSeed = Node "/" 0 []
 -- [size] [name]  -> create a leaf
 -- ---------------------------------------------------------------------------
 parser :: RoseTree -> [String] -> RoseTree
-parser t s = go t s []
+parser t s = go t s ""
   where
-    go tree ("$ cd /" : xs) _ = go tree xs ["/"]
-    go tree ("$ cd .." : xs) (_ : zs) = go tree xs zs
-    go tree (x : xs) stack@(z : zs) = case words x of
-      ["$", "cd", p] -> go tree xs (p : stack)
+    go tree ("$ cd /" : xs) _ = go tree xs "/"
+    go tree ("$ cd .." : xs) stack = go tree xs (pop stack)
+    go tree (x : xs) stack = case words x of
+      ["$", "cd", p] -> go tree xs (push p stack)
       ["$", "ls"]    -> go tree xs stack
-      ["dir", p]     -> go (insert tree (z, p, 0)) xs stack
-      [size, name]   -> go (insert tree (z, name, read size :: Int)) xs stack
+      ["dir", p]     -> go (insert tree (stack, push p stack, 0)) xs stack
+      [size, name]   -> go (insert tree (stack, push name stack, read size :: Int)) xs stack
       _              -> error "bork!"
-    go tree _ [] = tree
     go tree [] _ = tree
 
 main :: IO ()
 main = do
   inputs <- getContents
-  print $ parser rootSeed $ lines inputs
+  -- part one
+  print $ sum . filterTree (<= 100000) . sumTree . parser rootSeed $ lines inputs
